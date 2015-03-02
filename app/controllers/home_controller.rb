3deaf473565c23abf1b8e36c418eeafc11b126b0
= <jcentonzio@gmail.com>
2015-03-02 18:26:29 -0300

    Trabajando vista directorio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
   end
 
   def show
+  	 @directory = Directory.find(params[:id])
+  	 #ratings = Model.select(:rating).uniq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
    get 'accesses/new/:id' => 'accesses#new'
+   get 'show/:id' => 'home#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
